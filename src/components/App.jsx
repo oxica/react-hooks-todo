94ab8285b80164181f031b8b907af1aa8f76451e
Oksana <kskaplun@gmail.com>
2022-06-09 19:02:51 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
+import { useState } from 'react';
+
 export const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+
+  const AddTodo = () => {
+    if (text.trim().length) {
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text,
+          complited: false,
+        },
+      ]);
+    }
+    setText('');
+  };
+
+  const removeTodo = todoId => {
+    setTodos(todos.filter(todo => todo.id !== todoId));
+  };
+
+  const toggleTodoComplited = todoId => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id !== todoId) return todo;
+
+        return {
+          ...todo,
+          complited: !todo.complited,
+        };
+      })
+    );
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <center>
+      <label>
+        <input
+          className="text"
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+        <button onClick={AddTodo}>Add todo</button>
+      </label>
+
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.complited}
+              onChange={() => toggleTodoComplited(todo.id)}
+            />
+            <span>{todo.text}</span>
+            <span className="btn" onClick={() => removeTodo(todo.id)}>
+              &times;
+            </span>
+          </li>
+        ))}
+      </ul>
+    </center>
   );
 };
